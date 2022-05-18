761d0acc6cd5b693b1231c6141179dde1968d81f
smtono <64560893+smtono@users.noreply.github.com>
2022-05-17 23:13:56 -0500

    begin chat builder for npc

---
+++ src/admin_dev_tools/npc_chat_builder.py
@@ -0,0 +1,3 @@
+# We want to create a json file that contains all the npc chat data
+# This is used to create the npc chat data for the game
+# We will prompt the admin to enter the npc chat data, then provide all the response codes and the response translations
