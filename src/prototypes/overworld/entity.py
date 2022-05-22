1f71f266ce934e4ff5c37f1f439573464c30cb1f
smtono <64560893+smtono@users.noreply.github.com>
2022-05-21 19:58:26 -0500

    add enum for changing directions for player sprite

--- src/prototypes/overworld/entity.py
+++ src/prototypes/overworld/entity.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+import enum
 from typing import Any
 import pygame
 
@@ -99,9 +100,18 @@ class Player(Entity):
     The player will have a symbol and color
     It will also have functions for moving around the map
     '''
+
+    # make enum for each direction of movement, toggle the flag whenever user switches directions, use this flag to detect for collision
+    class Direction(enum):
+        UP = pygame.K_UP or pygame.K_w
+        DOWN = pygame.K_DOWN or pygame.K_s
+        LEFT = pygame.K_LEFT or pygame.K_a
+        RIGHT = pygame.K_RIGHT or pygame.K_d
+
     # Constructor
     def __init__(self) -> None:
         super().__init__()
+        self.is_moving = False
 
     # Movement functions
     '''
