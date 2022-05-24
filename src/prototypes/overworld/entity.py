a4af1bef7e765cf8d3e33bd112742f891f4e725f
smtono <64560893+smtono@users.noreply.github.com>
2022-05-24 01:31:55 -0500

    move direction enum to game.py

--- src/prototypes/overworld/entity.py
+++ src/prototypes/overworld/entity.py
@@ -101,13 +101,6 @@ class Player(Entity):
     It will also have functions for moving around the map
     '''
 
-    # make enum for each direction of movement, toggle the flag whenever user switches directions, use this flag to detect for collision
-    class Direction(enum):
-        UP = pygame.K_UP or pygame.K_w
-        DOWN = pygame.K_DOWN or pygame.K_s
-        LEFT = pygame.K_LEFT or pygame.K_a
-        RIGHT = pygame.K_RIGHT or pygame.K_d
-
     # Constructor
     def __init__(self) -> None:
         super().__init__()
--- src/prototypes/overworld/game.py
+++ src/prototypes/overworld/game.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import pygame, sys
 
 pygame.init()
@@ -51,4 +52,14 @@ class Camera:
     def reset(self):
         self.x = 0
         self.y = 0
-        
+
+# make enum for each direction of movement, toggle the flag whenever user switches directions, use this flag to detect for collision
+class Direction(Enum):
+    '''
+    Used for keeping track of the direction of the user in the overworld
+    '''
+    UP = pygame.K_UP or pygame.K_w
+    DOWN = pygame.K_DOWN or pygame.K_s
+    LEFT = pygame.K_LEFT or pygame.K_a
+    RIGHT = pygame.K_RIGHT or pygame.K_d
+    NONE = None
