db6e9c26a5fec59dd4c8d8f374a546b7537c94ab
smtono <64560893+smtono@users.noreply.github.com>
2022-05-25 18:08:27 -0500

    add placeholder dialogue.txt
    for now put text in here

---
+++ src/prototypes/overworld/dialogue.py
@@ -0,0 +1,9 @@
+"""
+something happens
+- [chat]
+    - response ->SELECT_RESPONSE: 48
+        [different chat options for response 48]
+            - response
+                [ different chat options ]
+                EXIT_CHAT -> "cya l8r alig8r"
+"""
