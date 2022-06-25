d93926c0c1d9fd704d3b319ca1c65180935c8d1c
smtono <64560893+smtono@users.noreply.github.com>
2022-06-25 17:31:27 -0500

    Add simple tests classes for movement and dialogue

--- src/tests/movement_tests.py
+++ src/tests/movement_tests.py
@@ -1,1 +1,6 @@
 import unittest
+
+# create a unit test to test the movement of the player from src\prototypes\overworld\movement.py
+class MovementTests(unittest.TestCase):
+    def test_movement(self):
+        pass
