8594c5e40714b3ba8851df136f2f2db8a8d68f62
smtono <64560893+smtono@users.noreply.github.com>
2022-07-04 16:45:15 -0500

    add simple state machine for input

--- src/prototypes/player/input.py
+++ src/prototypes/player/input.py
@@ -3,8 +3,49 @@ This module contains the Input class.
 
 Input in the overworld is a sequence of inputs made by the user.
 These inputs can correspond to different actions, such as moving, interacting, etc.
-This class serves as a container for the user's input, and a state machine to handle the input correctly and predictably.
+This class serves as a container for the user's input, 
+and a state machine to handle the input correctly and predictably.
 """
+from typing import Sequence
+import pygame
+
+class StateMachine():
+    """
+    State machine class to handle the input states.
+    
+    Attributes:
+        states: Sequence[str]
+            The possible states of the input.
+        states_transitions: Dict[str, str]
+            A dictionary mapping the states to the states that can be transitioned to from that state.
+        current_state: str
+            The current state of the input.
+    """
+    states = {
+            "idle": [pygame.K_d, pygame.K_a, pygame.K_w, pygame.K_s],
+            "moving": [pygame.K_d, pygame.K_a, pygame.K_w, pygame.K_s],
+            "interacting": [pygame.K_e],
+            "interacting_npc": [pygame.K_e]
+        }
+    states_transitions = {
+        "idle": "idle",
+        "moving": "idle",
+        "interacting": "idle",
+        "interacting_npc": "idle"
+    }
+        
+    def __init__(self) -> None:
+        """
+        Initialize the state machine class.
+        
+        Args:
+            states: Sequence[str]
+                The possible states of the input.
+            transitions: Dict[str, str]
+                A dictionary mapping the states to the states that can be transitioned to from that state.
+        """
+        self.current_state = self.states[0]
+        self.state_machine = StateMachine
 
 class Input():
     """
@@ -24,3 +65,13 @@ class Input():
         states_transitions: Dict[str, str]
             A dictionary mapping the states to the states that can be transitioned to from that state.
     """
+    def __init__(self) -> None:
+        """
+        Initialize the input class.
+        """
+        self.player = None
+        self.keys = []
+        self.state = "idle"
+        self.states = ["idle", "moving", "interacting", "interacting_npc"]
+        self.state_machine = StateMachine(self.states, self.states)
+    
