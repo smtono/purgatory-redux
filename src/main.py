5f2e16b6f5fe7b9b4df36192b72787dadd4a0c72
tono || トノ <64560893+smtono@users.noreply.github.com>
2022-08-22 14:13:21 -0500

    GENERAL: begin work on main file execution

---
+++ src/main.py
@@ -0,0 +1,6 @@
+"""
+This module serves as the central hub for the program.
+Program execution begins here, the user can choose to either
+run tools, or run prototypes currently in the program.
+"""
+
