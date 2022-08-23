f2354bcd38a2fb861f6784712f25b36f6533a594
tono || トノ <64560893+smtono@users.noreply.github.com>
2022-08-23 14:05:24 -0500

    GENERAL: add main interface for entering program

--- src/main.py
+++ src/main.py
@@ -4,3 +4,14 @@ Program execution begins here, the user can choose to either
 run tools, or run prototypes currently in the program.
 """
 
+print("Welcome to Purgatory Tools and Prototypes!")
+print("To begin, please preface with 'run' alongside the tool or prototype you wish to run.")
+print("\nTools:")
+print(
+    "\tdialogue cli - runs a dialogue system using the command line interface"
+    "\n\tWIP"
+)
+print("\nPrototypes:")
+print(
+    "\tmovement - a prototype for a movement system using blocks and entities"
+)
