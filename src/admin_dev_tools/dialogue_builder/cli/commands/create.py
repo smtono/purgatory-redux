2e733e66a5344daeaf8693cb28f62e87e4087576
tono || トノ <64560893+smtono@users.noreply.github.com>
2022-10-07 15:37:06 -0500

    finish command module for create function

--- src/admin_dev_tools/dialogue_builder/cli/commands/create.py
+++ src/admin_dev_tools/dialogue_builder/cli/commands/create.py
@@ -1,10 +1,14 @@
 """
-This module serves as the class definition for the Create class.
+This module serves as the class definition for the Create class.\
+
+Usage:
+    create type [id]
 """
 
 import os
-from admin_dev_tools.dialogue_builder.cli.commands.command import Command
 
+import admin_dev_tools.dialogue_builder.cli.util.dialogue_cli_util as util
+from admin_dev_tools.dialogue_builder.cli.commands.command import Command
 
 class Create(Command):
     """
@@ -29,9 +33,9 @@ class Create(Command):
         super().__init__()
         self.name = "create"
         self.description = "Create a new instance of NPC or scene."
-        self.usage = "create npc|scene [id]"
+        self.usage = "create type [id]"
         self.args = args
-    
+
     def syntax_check(self, args: list) -> bool:
         """
         Checks if the arguments are valid, raises errors if not
@@ -42,21 +46,20 @@ class Create(Command):
         Returns:
             True if the arguments are valid, False otherwise
         """
+        types = ["npc", "scene"]
+
         if len(args) == 0:
             print("Error: No arguments provided.")
             return False
         if len(args) > 2:
             print("Error: Too many arguments provided.")
             return False
-        if args[0] != "npc" and args[0] != "scene":
-            print("Error: Invalid argument provided.")
+        if args[0] not in types:
+            print(f"Error: {args[0]} Type does not exist.")
             return False
         if len(args) == 2:
-            try:
-                int(args[1])
-            except ValueError:
-                print("Error: Invalid argument provided.")
-                return False
+            # check if id is valid
+            pass
         return True
 
     def execute(self, args: list) -> dict:
@@ -70,3 +73,114 @@ class Create(Command):
             The created NPC or scene
         """
         os.system('cls')
+
+        if args[0] == "npc":
+            return self.create_npc()
+        if args[0] == "scene":
+            npc_id = "0000"
+        while npc_id not in npc_data:
+            print("Please enter the NPC ID that this scene is associated with")
+            npc_id = util.prompt_id()
+            if npc_id in npc_data:
+                scene = self.create_scene(npc_id)
+                break
+            print("This NPC does not exist")
+            print("Trying again. . .")
+
+  # TODO: maybe separate this into util class?
+    def create_npc(self) -> dict:
+        """
+        Creates a new instance of an NPC
+
+        Args:
+            None
+        Returns:
+            A dict containing information on the new NPC
+        """
+        npc = {
+            "id": "",
+            "name": "",
+            "portrait": "",
+            "type": "",
+            "mood": "",
+            "actions": {
+                "START_SESSION": {
+                    "good": [
+                        ""
+                    ],
+                    "neutral": [
+                        ""
+                    ],
+                    "bad": [
+                        ""
+                    ]
+                },
+                "END_SESSION": {
+                    "good": [
+                        ""
+                    ],
+                    "neutral": [
+                        ""
+                    ],
+                    "bad": [
+                        ""
+                    ]
+                }
+            },
+        }
+        print("Creating a new NPC")
+
+        # ID
+        while True:
+            npc_id = util.prompt_id()
+            if npc_id:
+                npc['id'] = npc_id
+            else:
+                npc_id = util.find_next_id(npc_data)
+                print(f"Creating an NPC with ID '{npc_id}'")
+                break
+
+        # Name
+        npc['name'] = util.prompt_string("Please enter the NPC's name", False)
+
+        # Portrait
+        # Type
+        npc_types = ['generic', 'quest_giver', 'shopkeeper', 'enemy']
+        while True:
+            npc_type = util.prompt_string(
+                "Please enter the NPC's type. For a list of types enter 'types'",
+                False
+            )
+            if npc_type == 'types':
+                print("NPC Types:")
+                print("\t", npc_types)
+            elif npc_type in npc_types:
+                npc['type'] = npc_type
+                break
+            else:
+                print(f"Input '{npc_type}' is not a valid type. Trying again. . .")
+
+        # Mood
+        while True:
+            print(
+                "\nPlease enter the starting integer for the mood of this NPC, from -10 to 10"
+                "\nPositive integers means positive mood, "
+                "negative means a starting negative mood:"
+            )
+            npc_mood = util.prompt_number(False)
+            if float(npc_mood) > 10 or float(npc_mood) < -10:
+                print("Input must be between -10 and 10. Trying again. . .")
+            else:
+                npc['mood'] = npc_mood
+                break
+
+        # Actions
+        print("\nNow adding dialogue actions to this NPC. . .")
+        actions = self.add_actions()
+
+        # Scenes
+        print("\nNow adding dialogue trees to this NPC. . .")
+        scenes = self.create_scene(npc_id)
+
+        print("Now exiting NPC creation. . .")
+        return npc
